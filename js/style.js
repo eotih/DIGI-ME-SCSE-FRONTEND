141dddae8aadfcc51981bff6ec841ac63f621164
My Nguyen <myoutybay@gmail.com>
2021-09-29 17:44:46 +0700

    nav 2nd fix

--- js/style.js
+++ js/style.js
@@ -1,6 +1,8 @@
 $(document).scroll(function(){
+    var elem = document.getElementById("slogan");
+    var x = elem.offsetTop -10;
     var y = $(this).scrollTop();
-    if(y>'2rem'){
+    if(y>x){
         $('nav').fadeIn();
     }else{
         $('nav').fadeOut();
