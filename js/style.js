c0e66b9f5c880cabb06c400b797bb6d1434e6d54
My Nguyen <myoutybay@gmail.com>
2021-10-04 16:25:53 +0700

    donate

--- js/style.js
+++ js/style.js
@@ -8,3 +8,41 @@ $(document).scroll(function(){
         $('nav').fadeOut();
     }
 });
+
+window.addEventListener('load', getDateTime)
+    async function getDateTime() {
+        var date = new Date();
+        var current_day = date.getDay();
+        var day_name = '';
+        switch (current_day) {
+            case 0:
+                day_name = "CHỦ NHẬT";
+                break;
+            case 1:
+                day_name = "THỨ HAI";
+                break;
+            case 2:
+                day_name = "THỨ BA";
+                break;
+            case 3:
+                day_name = "THỨ TƯ";
+                break;
+            case 4:
+                day_name = "THỨ NĂM";
+                break;
+            case 5:
+                day_name = "THỨ SÁU";
+                break;
+            case 6:
+                day_name = "THỨ BẢY";
+        }
+
+
+        var myVar = setInterval(function () {
+            var today = new Date();
+            var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+            var dateTime = day_name + ', ' + date + ' ' + time;
+            document.getElementById("datetime").innerText = dateTime;
+        }, 500);
+    }
