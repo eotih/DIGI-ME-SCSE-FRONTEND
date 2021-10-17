e0b696fa15563fa3710acf82a9caf0ff8db3b8f0
Le Trong Hieu <eotihvn@gmail.com>
2021-10-17 11:14:20 +0700

    done dktnv + liên hệ

---
+++ Utils/DangKyTinhNguyenVien.js
@@ -0,0 +1,32 @@
+const WEB_API = "http://localhost:59360/API/"
+
+function addVolunteer(){
+    var $data = {
+        FirstName: $('#FirstName').val(),
+        LastName: $('#LastName').val(),
+        DOB: $('#DOB').val(),
+        Phone: $('#Phone').val(),
+        Email: $('#Email').val(),
+        Address: $('#Address').val(),
+        Project: $('#Project').val(),
+        Purpose: $('#Purpose').val()
+    };
+    fetch(WEB_API + "Management/RegisterVolunteer", {
+        method: 'POST',
+        body: JSON.stringify($data),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+        },
+    }).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            if (data.Status === 'Success') {
+                alert('Thêm Thành Công')
+                window.location.reload();
+            }
+            else {
+                alert('Data not insert')
+            }
+        })
+}
