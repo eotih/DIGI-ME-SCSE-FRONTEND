062243d3c239fab516014b5bcea7014d3f3a66ab
My Nguyen <myoutybay@gmail.com>
2021-10-26 18:22:08 +0700

    fix thu vien hinh anh

--- Utils/ThuVienHinhAnh.js
+++ Utils/ThuVienHinhAnh.js
@@ -25,25 +25,21 @@ async function loadData() {
                         IDCat = 'Hợp tác nghiên cứu'
                     }
                     return `
-                    <div class="card mb-3" style="width:100%;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img style="width:300px;height:220px" src="${Image}" class=" mt-3 mx-3"
-                                style="border-radius: 2rem;" alt="...">
-                        </div>
-                        <div class="col-md-8">
+                    <div class="col-lg-4 d-flex align-items-stretch">
+                    <div class="mb-5">
+                        <div class="card" style="width:100%; height:100%;box-shadow: 10px 10px 0px #C4C4C4;">
+                                <img id="ImgAlbum" onclick="getImage('${Slug}')" src="${Image}"
+                                    class="card-img-top my-2 px-2" style="width:350px; height:250px;object-fit:cover" alt="...">
                             <div class="card-body">
-                                <a href="../Chi-Tiet-Bai-Viet/index.html?slug=${SlugEN}">
-                                    <h5 class="card-title font-weight-bold">${Title}</h5>
+                                <a style="text-decoration: none; color:black" href="#">
+                                    <h5 class="card-title">${Title}</h5>
                                 </a>
-                                <p class="card-text"><small class="text-muted">${CreatedByDate}</small></p>
-                                <p class="card-text">${Details.slice(0, 500)}</p>
 
-                                <a href="../Chi-Tiet-Bai-Viet/index.html?slug=${SlugEN}"><button
-                                        class="btn bg-blue-scse btn-primary lg">Chi Tiet</button></a>
+                            </div>
+                            <div class="card-footer">
+                                <small class="text-muted">${IDCat}</small>
                             </div>
                         </div>
-
                     </div>
                 </div>
                     `;
@@ -97,14 +93,14 @@ function getImage(Slug) {
         const data = filterData.slice(0, filterData.length-1).map(function (response) {
             return `
                 <div class="carousel-item">
-                            <img class="d-block mx-auto" src="${response.Image}">
+                            <img class="d-block w-50 mx-auto" src="${response.Image}">
                         </div>
                 `;
         })
         const html = filterData.slice(filterData.length-1).map(function (response) {
             return `
                 <div class="carousel-item active">
-                            <img class="d-block mx-auto" src="${response.Image}">
+                            <img class="d-block w-50 mx-auto" src="${response.Image}">
                         </div>
                 `;
         })
