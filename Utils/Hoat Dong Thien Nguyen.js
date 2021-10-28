7d8d0ac214eef6b80e8824c0c34bb27ffb36061e
Le Trong Hieu <eotihvn@gmail.com>
2021-10-28 18:55:58 +0700

    abc

--- Utils/Hoat Dong Thien Nguyen.js
+++ Utils/Hoat Dong Thien Nguyen.js
@@ -5,64 +5,21 @@ function convertDate(input) {
     return result.toLocaleDateString()
 }
 async function loadHDTN(numb) {
-    if(numb == 1){
+    if (numb == 1) {
         fetch(WEB_API + "API/Management/GetByIdCategory?idcat=3")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            var postApproved = response.filter(e => e.IDState === 2)
-            const sortByNewDate = postApproved.sort(function (a, b) {
-                a = new Date(a.UpdatedByDate);
-                b = new Date(b.UpdatedByDate);
-                return a > b ? -1 : a < b ? 1 : 0;
+            .then(function (response) {
+                return response.json();
             })
-            const loadMore = $('#loadMore')
-                loadMore.on('click', function (e) {
-                    e.preventDefault();
-                    var html = sortByNewDate.map(function (response) {
-                        const { IDPost, Title, Slug, Details, Image, CreatedByDate } = response
-                        return `
-                        <div class="card mb-3" style="width:100%; border-radius: 10px">
-                        <div class="row g-0" style="background-color: #F0F0F0">
-                            <div class="col-lg-4 p-0">
-                                <div class="d-flex justify-content">
-                                    <div class="bg-red-scse" style="
-                                    padding-right:20px;
-                                        height: 400px;
-                                    "></div>
-                                    <img src="${Image}"
-                                        style="width: 350px;height: 370px;border-radius: 10px;object-fit: cover;"
-                                        class="img-fluid my-3" alt="...">
-                                </div>
-                            </div>
-        
-                            <div class="col-lg-8">
-                                <div class="card-body">
-                                    <a href="../Chi-Tiet-Bai-Viet/index.html?slug=${Slug}">
-                                    <h3 class="card-title text-muted">${Title}</h3>
-                                    </a>
-                                    <p class="card-text"><small class="text-muted">${convertDate(CreatedByDate)}</small></p>
-                                    <p class="card-text" style="text-align: justify; text-justify: inter-word">${Details.slice(0,300)}...</p>
-                                    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                        <a href="../Dang-Ky-Tinh-Nguyen-Vien/" style="color:#ffffff" type="button"
-                                            class="btn bg-red-scse btn-danger lg">Đăng ký</a>
-        
-                                    </div>
-                                </div>
-                            </div>
-        
-                        </div>
-                    </div>
-                `;
-                    })
-                    $('#tbody').html(html);
-                    $(".loader-wrapper").fadeOut("slow");
+            .then(function (response) {
+                const postApproved = response.filter(e => e.IDState === 2)
+                const sortByNewDate = postApproved.sort(function (a, b) {
+                    a = new Date(a.UpdatedByDate);
+                    b = new Date(b.UpdatedByDate);
+                    return a > b ? -1 : a < b ? 1 : 0;
                 })
-                const get6NewestPostedDate = sortByNewDate.slice(0, 6)
-            var html = get6NewestPostedDate.map(function (response) {
-                const { IDPost, Title, Slug, Details, Image, CreatedByDate } = response
-                return `
+                const html = sortByNewDate.map(function (response) {
+                    const { IDPost, Title, Slug, Details, Image, CreatedByDate } = response
+                    return `
                 <div class="card mb-3" style="width:100%; border-radius: 10px">
                         <div class="row g-0" style="background-color: #F0F0F0">
                             <div class="col-lg-4 p-0">
@@ -83,7 +40,7 @@ async function loadHDTN(numb) {
                                     <h3 class="card-title text-muted">${Title}</h3>
                                     </a>
                                     <p class="card-text"><small class="text-muted">${convertDate(CreatedByDate)}</small></p>
-                                    <p class="card-text" style="text-align: justify; text-justify: inter-word">${Details.slice(0,300)}...</p>
+                                    <p class="card-text" style="text-align: justify; text-justify: inter-word">${Details.slice(0, 300)}...</p>
                                     <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                                         <a href="../Dang-Ky-Tinh-Nguyen-Vien/" style="color:#ffffff" type="button"
                                             class="btn bg-red-scse btn-danger lg">Đăng ký</a>
@@ -95,23 +52,31 @@ async function loadHDTN(numb) {
                         </div>
                     </div>
                  `;
+                })
+                $('#list').pagination({
+                    dataSource: html,
+                    pageSize: 6,
+                    callback: function (data, pagination) {
+                        $(".loader-wrapper").fadeOut("slow");
+                        $('#tbody').html(data);
+            
+                    }
+                })
             })
-            $('#tbody').html(html);
-        })
     }
     else {
         fetch(WEB_API + "API/Management/GetByIdCategoryEN?idcat=3")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            var postApproved = response.filter(e => e.IDState === 2)
-            const sortByNewDate = postApproved.sort(function (a, b) {
-                a = new Date(a.UpdatedByDate);
-                b = new Date(b.UpdatedByDate);
-                return a > b ? -1 : a < b ? 1 : 0;
+            .then(function (response) {
+                return response.json();
             })
-            const loadMore = $('#loadMore')
+            .then(function (response) {
+                var postApproved = response.filter(e => e.IDState === 2)
+                const sortByNewDate = postApproved.sort(function (a, b) {
+                    a = new Date(a.UpdatedByDate);
+                    b = new Date(b.UpdatedByDate);
+                    return a > b ? -1 : a < b ? 1 : 0;
+                })
+                const loadMore = $('#loadMore')
                 loadMore.on('click', function (e) {
                     e.preventDefault();
                     var html = sortByNewDate.map(function (response) {
@@ -154,9 +119,9 @@ async function loadHDTN(numb) {
                     $(".loader-wrapper").fadeOut("slow");
                 })
                 const get6NewestPostedDate = sortByNewDate.slice(0, 6)
-            var html = get6NewestPostedDate.map(function (response) {
-                const { IDPostEN, Title, SlugEN, Details, Image, CreatedByDate } = response
-                return `
+                var html = get6NewestPostedDate.map(function (response) {
+                    const { IDPostEN, Title, SlugEN, Details, Image, CreatedByDate } = response
+                    return `
                 <div class="card mb-3" style="width:100%; border-radius: 10px">
                 <div class="row g-0" style="background-color: #F0F0F0">
                     <div class="col-lg-4 p-0">
@@ -190,9 +155,9 @@ async function loadHDTN(numb) {
                 </div>
             </div>
                  `;
+                })
+                $('#tbody').html(html);
             })
-            $('#tbody').html(html);
-        })
     }
-    
+
 }
